45b2ac97dbdc984c9fc7480fd9b03779e98d4276
SJagenow <sjagenow@web.de>
2024-11-13 13:45:36 +0100

    MarketsView class mixins

--- market_app/api/urls.py
+++ market_app/api/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import markets_view, market_single_view, products_view,sellers_view,seller_single_view
+from .views import markets_view, market_single_view, products_view,sellers_view,seller_single_view,MarketsView
 
 urlpatterns = [
-    path('market/', markets_view),
+    path('market/', MarketsView.as_view()),
     path('market/<int:pk>/', market_single_view , name='market-detail'),
     path('seller/', sellers_view),
     path('product/', products_view),
--- market_app/api/views.py
+++ market_app/api/views.py
@@ -3,6 +3,24 @@ from rest_framework.response import Response
 from rest_framework import status
 from .serializers import MarketHayperlinkedSerializer, MarketSerializer, ProductSerializer, SellerSerializer
 from market_app.models import Market, Product, Seller
+from rest_framework.views import APIView
+from rest_framework import mixins
+from rest_framework import generics
+
+class MarketsView(mixins.ListModelMixin,
+                  mixins.CreateModelMixin,
+                  generics.GenericAPIView):
+   
+   queryset = Market.objects.all()
+   serializer_class = MarketSerializer
+
+   def get(self, request, *args, **kwargs):
+    return self.list(request, *args, **kwargs)
+
+   def post(self, request, *args, **kwargs):
+    return self.create(request, *args, **kwargs)
+
+
 
 
 @api_view(['GET','POST']) #decorator
