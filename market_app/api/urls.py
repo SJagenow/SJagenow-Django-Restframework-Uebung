447c237da17bd53a881355f8ecbd8464300272a7
SJagenow <sjagenow@web.de>
2024-11-13 14:46:32 +0100

    generics

--- market_app/api/urls.py
+++ market_app/api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import markets_view, market_single_view, products_view,sellers_view,seller_single_view,MarketsView
+from .views import MarketsSingleView, products_view,sellers_view,seller_single_view,MarketsView,SellerOfMarketList
 
 urlpatterns = [
     path('market/', MarketsView.as_view()),
-    path('market/<int:pk>/', market_single_view , name='market-detail'),
+    path('market/<int:pk>/', MarketsSingleView.as_view() , name='market-detail'),
+    path('market/<int:pk>/sellers/', SellerOfMarketList.as_view()),
     path('seller/', sellers_view),
     path('product/', products_view),
     path('seller/<int:pk>/', seller_single_view, name='seller_single')
--- market_app/api/views.py
+++ market_app/api/views.py
@@ -4,66 +4,107 @@ from rest_framework import status
 from .serializers import MarketHayperlinkedSerializer, MarketSerializer, ProductSerializer, SellerSerializer
 from market_app.models import Market, Product, Seller
 from rest_framework.views import APIView
-from rest_framework import mixins
-from rest_framework import generics
+from rest_framework import mixins,generics
 
-class MarketsView(mixins.ListModelMixin,
-                  mixins.CreateModelMixin,
-                  generics.GenericAPIView):
+
+
+
+class MarketsView(generics.ListCreateAPIView):
    
    queryset = Market.objects.all()
    serializer_class = MarketSerializer
 
-   def get(self, request, *args, **kwargs):
-    return self.list(request, *args, **kwargs)
+#    def get(self, request, *args, **kwargs):
+#     return self.list(request, *args, **kwargs)
 
-   def post(self, request, *args, **kwargs):
-    return self.create(request, *args, **kwargs)
-
-
+#    def post(self, request, *args, **kwargs):
+#     return self.create(request, *args, **kwargs)
 
 
-@api_view(['GET','POST']) #decorator
-def markets_view(request):
-    if request.method == 'GET':
-      markets = Market.objects.all()
-      serializer = MarketHayperlinkedSerializer(markets, many=True,context={'request': request},fields=('id','url','name'))
-      return Response(serializer.data)
+
+
+# @api_view(['GET','POST']) #decorator
+# def markets_view(request):
+    # if request.method == 'GET':
+    #   markets = Market.objects.all()
+    #   serializer = MarketHayperlinkedSerializer(markets, many=True,context={'request': request},fields=('id','url','name'))
+    #   return Response(serializer.data)
     
 
-    if request.method == 'POST':
-       serializer = MarketSerializer(data=request.data)
-       if serializer.is_valid():
-          serializer.save()
-          return Response(serializer.data)
-       else:
-          return Response(serializer.errors)
+    # if request.method == 'POST':
+    #    serializer = MarketSerializer(data=request.data)
+    #    if serializer.is_valid():
+    #       serializer.save()
+    #       return Response(serializer.data)
+    #    else:
+    #       return Response(serializer.errors)
 
 
 
 
-@api_view(['GET', 'DELETE', 'PUT']) #decorator
-def market_single_view(request, pk):
 
-    if request.method == 'GET':
-      market = Market.objects.get(pk=pk)
-      serializer = MarketSerializer(market,context={'request': request})
-      return Response(serializer.data)
+
+
+# class MarketsSingleView(mixins.RetrieveModelMixin,
+#                     mixins.UpdateModelMixin,
+#                     mixins.DestroyModelMixin,
+#                     generics.GenericAPIView):
+   
+#     queryset = Market.objects.all()
+#     serializer_class = MarketSerializer
+
+#     def get(self, request, *args, **kwargs):
+#      return self.retrieve(request, *args, **kwargs)
+
+#     def put(self, request, *args, **kwargs):
+#      return self.update(request, *args, **kwargs)
+
+#     def delete(self, request, *args, **kwargs):
+#      return self.destroy(request, *args, **kwargs)
+
+class MarketsSingleView(generics.RetrieveUpdateDestroyAPIView):
+   
+     queryset = Market.objects.all()
+     serializer_class = MarketSerializer
+
+
+class SellerOfMarketList(generics.ListAPIView):
+   serializer_class = SellerSerializer
+
+   def get_queryset(self):
+       pk = self.kwargs.get('pk')
+       market = Market.objects.get(pk = pk)
+      
+       return market.sellers.all()
+
+
+
+
+
+
+
+# @api_view(['GET', 'DELETE', 'PUT']) #decorator
+# def market_single_view(request, pk):
+
+#     if request.method == 'GET':
+#       market = Market.objects.get(pk=pk)
+#       serializer = MarketSerializer(market,context={'request': request})
+#       return Response(serializer.data)
     
-    if request.method == 'PUT':
-      market = Market.objects.get(pk=pk)
-      serializer = MarketSerializer(market, data=request.data, partial=True)
-      if serializer.is_valid():
-          serializer.save()
-          return Response(serializer.data)
-      else:
-          return Response(serializer.errors)
+#     if request.method == 'PUT':
+#       market = Market.objects.get(pk=pk)
+#       serializer = MarketSerializer(market, data=request.data, partial=True)
+#       if serializer.is_valid():
+#           serializer.save()
+#           return Response(serializer.data)
+#       else:
+#           return Response(serializer.errors)
 
-    if request.method == 'DELETE':
-       market = Market.objects.get(pk=pk)
-       serializer = MarketSerializer(market)
-       market.delete()
-       return Response(serializer.data)
+#     if request.method == 'DELETE':
+#        market = Market.objects.get(pk=pk)
+#        serializer = MarketSerializer(market)
+#        market.delete()
+#        return Response(serializer.data)
     
    
 
